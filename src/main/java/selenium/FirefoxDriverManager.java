9a15c5c0dc1cadeff293587f8e8276674a2efc7d
SasaStjepanovic <stjepanovics80@yahoo.com>
2023-05-29 19:48:36 +0200

    POM model is added

---
+++ src/main/java/selenium/FirefoxDriverManager.java
@@ -0,0 +1,17 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirefoxDriverManager extends DriverManager{
+    @Override
+    public void createWebDriver() {
+        WebDriverManager.firefoxdriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+
+        driver = new ChromeDriver(options);
+    }
+}
